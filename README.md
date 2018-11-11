b5db96da3955db3b0baf063fa460817fed3b53ba
jrbreckenitch <jamesbreckenitch@Jamess-MacBook-Pro.local>
2018-11-10 21:13:14 -0600

    app can scrape articles and update and delete from db

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # News-Scraper
+
+Heroku Link
+
+https://frozen-falls-47945.herokuapp.com/
+
+Despcription
+
+This is an application that scrapes the Hacker News website.  When you click the Scrape New Article button a list of articles with titles will be scraped from Hacker News (currently the page needs to be refreshed to view the list of articles). The user can then click the Clear Articles button, which will remove all articles from the database that had previously been scraped.
--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect("mongodb://localhost/newsArticles", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/newsArticles", { useNewUrlParser: true });
 
-// mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
